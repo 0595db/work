a0404856d4e9cacc3df87a3cbd28866de27538b0
0595db <0595db@0595dbs-MacBook-Pro.local>
2015-11-01 11:52:12 +0800

    Sun Nov  1 11:52:03 CST 2015

--- pat/1002.c
+++ pat/1002.c
@@ -6,9 +6,8 @@ int main(void)
 	char xs[XS_MAX];
 	scanf("%s", xs);
 
-	int i;
 	int sum = 0;
-	for (i = 0; xs[i] != '\0'; i++) {
+	for (int i = 0; xs[i] != '\0'; i++) {
 		sum += (int)(xs[i] - '0');
 	}
 
@@ -16,7 +15,7 @@ int main(void)
 	char *base[] = {"ling", "yi", "er", "san", "si", "wu", "liu",
 	"qi", "ba", "jiu", };
 
-	for (i = 0; xs[i] != '\0'; i++) {
+	for (int i = 0; xs[i] != '\0'; i++) {
 		if (i != 0) {
 			putchar(' ');
 		}
--- pat/1004.c
+++ pat/1004.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 11
 
 struct info {
-	char name[11];
-	char ID[11];
-	int score;
+	char	name[MAX];
+	char	id[MAX];
+	int 	mark;
 };
 
 int compar(const void *pa, const void *pb);
 int main(void)
 {
 	int size;
-	scanf("%d", &size);
+	scanf("%d",&size);
 
-	struct info user[size];
-	int i;
-	for (i = 0; i < size; i++) {
-		scanf("%s %s %d", 
-		user[i].name, user[i].ID, &user[i].score);
+	struct info xs[size];
+
+	for (int i = 0; i < size; i++) {
+		scanf("%s %s %d", xs[i].name, xs[i].id, &xs[i].mark);
 	}
+	qsort(xs, size, sizeof(*xs), compar);
 
-	qsort(user, size, sizeof(*user), compar);
-	printf("%s %s\n", user[size - 1].name, user[size - 1].ID);
-	printf("%s %s\n", user[0].name, user[0].ID);
+	printf("%s %s\n", xs[size - 1].name, xs[size - 1].id);
+	printf("%s %s\n", xs[0].name, xs[0].id);
+
 	return 0;
 }
 
@@ -30,5 +31,5 @@ int compar(const void *pa, const void *pb)
 {
 	struct info a = *(struct info *)pa;
 	struct info b = *(struct info *)pb;
-	return a.score - b.score;
+	return a.mark - b.mark;
 }
